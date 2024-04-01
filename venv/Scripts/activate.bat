c065fb4b69dff1990f328bc5f6cdfb958516a629
Joey Schulz <kjap.schulz@gmail.com>
2024-03-31 20:45:04 -0700

    changed

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\s0340484\GitHub\AVATAR\venv
+set VIRTUAL_ENV=C:\Users\Joey\GitHub\AVATAR\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
